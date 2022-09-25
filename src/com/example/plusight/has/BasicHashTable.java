7abf1130710332ed6446f9321c62b8f952c49760
Nabil Nader <nabil.nader00@gmail.com>
2022-09-25 10:17:23 +0200

    done with size, and get method for hash

--- src/com/example/plusight/has/BasicHashTable.java
+++ src/com/example/plusight/has/BasicHashTable.java
@@ -6,10 +6,46 @@ public class BasicHashTable <X,Y>{
     //this will be how big is the hash table is
     private int capacity;
 
+    private int size;
+
     public BasicHashTable(int capacity) {
         this.capacity = capacity;
         this.data = new HashEntity[this.capacity];
+        this.size = 0;
     }
+
+    // create get size method
+
+    public int size(){
+        return this.size;
+    }
+    // create get/put method
+
+    public Y get(X key){
+
+        int hash = calculateHas(key);
+        // if we don't have anything for the given key, just return null
+        if(data[hash] == null){
+            return null;
+        }
+        //otherwise, get has the hasEntry for the key and retrurn its value
+        else {
+            return (Y)data[hash].getValue();
+        }
+    }
+    // before create get method we will create a private method
+
+    private int calculateHas(X key){
+        int hash = (key.hashCode() % this.capacity);
+        // this is necessary to deal with collisions.
+
+        while (data[hash] !=null && !data[hash].getKey().equals(key)){
+            hash = (hash + 1) % this.capacity;
+        }
+        return hash;
+    }
+
+
 
     public class HashEntity<X,Y>{
         private X key;
